8805cdb8dbd9e5bcbaef405b3a9a437b15043a0e
Meteor117 <kharitonov_62@mail.ru>
2024-11-04 22:19:27 +0300

    Add files via upload

---
+++ module_10_2.py
@@ -0,0 +1,49 @@
+#"Честь и отвага!"
+
+from threading import Thread
+from time import sleep
+class Knight(Thread):
+    def __init__(self, name: str, power: int):
+        super().__init__()
+        self.name = name
+        self.power = power
+
+    def run(self):
+        print(f"{self.name}, на нас напали!")
+        enemies = 100
+        days = 0
+        while enemies > 0:
+            sleep(1)
+            days += 1
+            enemies -= self.power
+
+            print(f"{self.name} сражается {days}..., осталось {enemies} воинов\n")
+
+        print(f"{self.name} одержал победу спустя {days} дней (дня)!")
+
+# Создание класса
+first_knight = Knight('Sir Lancelot', 10)
+second_knight = Knight("Sir Galahad", 20)
+# Запуск потоков и остановка текущего
+first_knight.start()
+second_knight.start()
+
+first_knight.join()
+second_knight.join()
+# Вывод строки об окончании сражения
+print("Все битвы закончились!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
